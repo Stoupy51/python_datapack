3ca4058ed445e807e95bac6651c913ca5f07dbe4
Stoupy51 <stoupy51@gmail.com>
2024-07-21 19:14:23 +0200

    🐛 Fixed auto uploader

--- 3_upload.py
+++ 3_upload.py
@@ -2,7 +2,8 @@
 import os
 LAST_FILES = 2
 for root, _, files in os.walk("dist"):
-	lasts = files[-2*LAST_FILES:]	# x2 because we have the .tar.gz and the .whl
+	files = sorted(files, key=lambda x: os.path.getmtime(f"dist/{x}"), reverse=True)
+	lasts = files[:LAST_FILES * 2] # x2 because we have the .tar.gz and the .whl
 	for file in lasts:
 		if file.endswith(".tar.gz"):
 			os.system(f"py -m twine upload --verbose -r python_datapack dist/{file}")
