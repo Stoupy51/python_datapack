a16a6bf6fc3c1577809bfea398c4b56807179e8c
Stoupy51 <stoupy51@gmail.com>
2024-11-29 14:26:34 +0100

    build: 🚀 Updated ItemIO to v1.3.2

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 
-# Package Link
+# 📦 Package Link
 [https://pypi.org/project/python-datapack/](https://pypi.org/project/python-datapack/)
 
-# Python Datapack
-Here is a template to a GitHub repository using this Python package:
+# 🐍 Python Datapack
+Here is a template to a GitHub repository using this Python package: 📝
 [https://github.com/Stoupy51/PythonDatapackTemplate](https://github.com/Stoupy51/PythonDatapackTemplate)
 
-Here is an advanced example using this Python package:
+Here is an advanced example using this Python package: ⚡
 [https://github.com/Stoupy51/SimplEnergy](https://github.com/Stoupy51/SimplEnergy)
 
--- src/python_datapack/constants.py
+++ src/python_datapack/constants.py
@@ -66,6 +66,6 @@ OFFICIAL_LIBS: dict[str, dict] = {
 	"smithed.crafter":		{"version":[0, 5, 0], "name":"Smithed Crafter",					"url":"https://wiki.smithed.dev/libraries/crafter/",		"is_used": False},
 	"furnace_nbt_recipes":	{"version":[1, 8, 0], "name":"Furnace NBT Recipes",				"url":"https://github.com/Stoupy51/FurnaceNbtRecipes",		"is_used": False},
 	"smart_ore_generation":	{"version":[1, 7, 1], "name":"SmartOreGeneration",				"url":"https://github.com/Stoupy51/SmartOreGeneration",		"is_used": False},
-	"itemio":				{"version":[1, 3, 1], "name":"ItemIO",							"url":"https://github.com/edayot/ItemIO",					"is_used": False},
+	"itemio":				{"version":[1, 3, 2], "name":"ItemIO",							"url":"https://github.com/edayot/ItemIO",					"is_used": False},
 }
 
