e8ec1649b7340325ba7364d0b91946f06bc352ad
Stoupy51 <stoupy51@gmail.com>
2025-01-23 17:25:13 +0100

    chore: 🔧 Add packaging to pip upgrade command in 1_upgrades.py

--- 1_upgrades.py
+++ 1_upgrades.py
@@ -1,5 +1,5 @@
 
 import os
 import sys
-os.system(f"{sys.executable} -m pip install --upgrade pip setuptools build twine")
+os.system(f"{sys.executable} -m pip install --upgrade pip setuptools build twine packaging")
 
