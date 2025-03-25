a3646bbf227e19d1f05507acd309c6a6b8a94705
Stoupy51 <stoupy51@gmail.com>
2025-03-25 16:49:16 +0100

    chore: 🔧 Added support for multiple destination paths in local copy script

--- copy_in_local.py
+++ copy_in_local.py
@@ -3,10 +3,19 @@ import shutil
 import os
 
 source = os.path.dirname(os.path.realpath(__file__)).replace("\\","/") + "/src/python_datapack"
-destination = "C:/Users/Alexandre-PC/AppData/Local/Programs/Python/Python312/Lib/site-packages/python_datapack"
+destinations: list[str] = [
+	"C:/Users/Alexandre-PC/AppData/Local/Programs/Python/Python312/Lib/site-packages/python_datapack",
+	"C:/Users/1053914/AppData/Local/Programs/Python/Python312/Lib/site-packages/python_datapack"
+]
 
-shutil.rmtree(destination, ignore_errors=True)
-shutil.copytree(source, destination)
+
+for dest in destinations:
+	try:
+		shutil.rmtree(dest, ignore_errors=True)
+		shutil.copytree(source, dest)
+	except Exception as e:
+		pass
+
 os.system("clear")
 print("\nCopied python_datapack to local Python's site-packages\n")
 
