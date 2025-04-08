42bb05c355678bbfe2c5074f8c77c4ecc42ccebe
Stoupy51 <stoupy51@gmail.com>
2025-04-08 21:44:06 +0200

    fix: 🐛 Fixed call to bookshelf load function tag

--- src/python_datapack/dependencies/main.py
+++ src/python_datapack/dependencies/main.py
@@ -9,7 +9,7 @@ OFFICIAL_LIBS_PATH: str = stp.get_root_path(__file__)
 
 # Util function for checking version
 def check_version(config: dict, lib_ns: str, data: dict, run_command: str) -> str:
-	namespace: str = config['namespace']
+	ns: str = config['namespace']
 	checks: str = ""
 	major, minor, patch = data["version"]
 	score_major: str = f"score #{lib_ns}.major load.status matches {major}"
@@ -24,21 +24,21 @@ def check_version(config: dict, lib_ns: str, data: dict, run_command: str) -> st
 
 	# Check if the version is correct
 	is_decoder: int = 1 if "tellraw @" in run_command else 0
-	checks += f"execute if score #dependency_error {namespace}.data matches {is_decoder} unless {score_major}.. run {run_command}\n"
-	checks += f"execute if score #dependency_error {namespace}.data matches {is_decoder} if {score_major} unless {score_minor}.. run {run_command}\n"
+	checks += f"execute if score #dependency_error {ns}.data matches {is_decoder} unless {score_major}.. run {run_command}\n"
+	checks += f"execute if score #dependency_error {ns}.data matches {is_decoder} if {score_major} unless {score_minor}.. run {run_command}\n"
 	if score_patch:
-		checks += f"execute if score #dependency_error {namespace}.data matches {is_decoder} if {score_major} if {score_minor} unless {score_patch}.. run {run_command}\n"
+		checks += f"execute if score #dependency_error {ns}.data matches {is_decoder} if {score_major} if {score_minor} unless {score_patch}.. run {run_command}\n"
 	return checks
 
 # Main function called on finalization process
 @stp.measure_time(stp.info, "Dependencies generated")
 def main(config: dict) -> None:
-	namespace: str = config['namespace']
+	ns: str = config['namespace']
 	version: str = config['version']
 	major, minor, patch = version.split(".")
 
 	# Find if common_signals is used
-	if namespace != "common_signals":
+	if ns != "common_signals":
 		for file_content in FILES_TO_WRITE.values():
 			if "common_signals" in file_content:
 				if not official_lib_used("common_signals"):
@@ -46,7 +46,7 @@ def main(config: dict) -> None:
 				break
 	
 	# Find if itemio is used
-	if namespace != "itemio":
+	if ns != "itemio":
 		for file_content in FILES_TO_WRITE.values():
 			if "itemio" in file_content:
 				if not official_lib_used("itemio"):
@@ -54,7 +54,7 @@ def main(config: dict) -> None:
 				break
 	
 	# Find for each bookshelf module if it is used
-	if namespace != "bookshelf":
+	if ns != "bookshelf":
 		for module_ns in BOOKSHELF_MODULES.keys():
 			for file_content in FILES_TO_WRITE.values():
 				if f"#{module_ns}:" in file_content:
@@ -63,7 +63,7 @@ def main(config: dict) -> None:
 					break
 
 	# Get all dependencies (official and custom)
-	dependencies: list[tuple] = [(ns, data) for ns, data in OFFICIAL_LIBS.items() if data["is_used"]]
+	dependencies: list[tuple[str, dict]] = [(ns, data) for ns, data in OFFICIAL_LIBS.items() if data["is_used"]]
 	if config.get("dependencies"):
 		dependencies += list(config['dependencies'].items())
 
@@ -79,14 +79,15 @@ scoreboard players reset * load.status
 
 
 	# Setup load json files
-	write_file(f"{config['build_datapack']}/data/load/tags/function/load.json", stp.super_json_dump({"values": [f"#{namespace}:load"]}))
-	values: list[str | dict] = [f"#{namespace}:enumerate", f"#{namespace}:resolve"]
+	write_file(f"{config['build_datapack']}/data/load/tags/function/load.json", stp.super_json_dump({"values": [f"#{ns}:load"]}))
+	values: list[str | dict] = [f"#{ns}:enumerate", f"#{ns}:resolve"]
 	if dependencies:
-		values.insert(0, {"id":f"#{namespace}:dependencies","required":False})
-	write_file(f"{config['build_datapack']}/data/{namespace}/tags/function/load.json", stp.super_json_dump({"values": values}, max_level = 3))
+		values.insert(0, {"id":f"#{ns}:dependencies","required":False})
+	write_file(f"{config['build_datapack']}/data/{ns}/tags/function/load.json", stp.super_json_dump({"values": values}, max_level = 3))
 	if dependencies:
-		calls = [{"id":f"#{namespace}:load", "required": False} for namespace, _ in dependencies]
-		write_file(f"{config['build_datapack']}/data/{namespace}/tags/function/dependencies.json", stp.super_json_dump({"values": calls}))
+		calls = [{"id":f"#{ns}:load" if not ns.startswith("bs.") else f"#bs.load:load", "required": False} for ns, _ in dependencies]
+		calls = stp.unique_list(calls)
+		write_file(f"{config['build_datapack']}/data/{ns}/tags/function/dependencies.json", stp.super_json_dump({"values": calls}))
 
 	# Write secondary function
 	authors = config['author'].split(" ")
@@ -110,19 +111,19 @@ function {config['namespace']}:v{version}/load/confirm_load
 
 
 	# Tick verification
-	if is_in_write_queue(f"{config['build_datapack']}/data/{namespace}/function/v{version}/tick.mcfunction"):
-		write_file(f"{config['build_datapack']}/data/minecraft/tags/function/tick.json", stp.super_json_dump({"values": [f"{namespace}:v{version}/load/tick_verification"]}))
+	if is_in_write_queue(f"{config['build_datapack']}/data/{ns}/function/v{version}/tick.mcfunction"):
+		write_file(f"{config['build_datapack']}/data/minecraft/tags/function/tick.json", stp.super_json_dump({"values": [f"{ns}:v{version}/load/tick_verification"]}))
 		write_versioned_function(config, "load/tick_verification", f"""
-execute if score #{namespace}.major load.status matches {major} if score #{namespace}.minor load.status matches {minor} if score #{namespace}.patch load.status matches {patch} run function {namespace}:v{version}/tick
+execute if score #{ns}.major load.status matches {major} if score #{ns}.minor load.status matches {minor} if score #{ns}.patch load.status matches {patch} run function {ns}:v{version}/tick
 
 """)
 
 	# Link smart_ore_generation library functions
 	if OFFICIAL_LIBS["smart_ore_generation"]["is_used"]:
 		for function_tag in ["denied_dimensions", "generate_ores", "post_generation"]:
-			function_path: str = f"{config['build_datapack']}/data/{namespace}/function/calls/smart_ore_generation/{function_tag}.mcfunction"
+			function_path: str = f"{config['build_datapack']}/data/{ns}/function/calls/smart_ore_generation/{function_tag}.mcfunction"
 			if FILES_TO_WRITE.get(function_path):
-				json_file: dict = {"values": [f"{namespace}:calls/smart_ore_generation/{function_tag}"]}
+				json_file: dict = {"values": [f"{ns}:calls/smart_ore_generation/{function_tag}"]}
 				path: str = f"{config['build_datapack']}/data/smart_ore_generation/tags/function/v1/signals/{function_tag}.json"
 				write_file(path, stp.super_json_dump(json_file))
 
@@ -138,7 +139,7 @@ execute if score #{namespace}.major load.status matches {major} if score #{names
 	for lib_ns, value in dependencies:
 
 		# Encoder check
-		encoder_command: str = f"scoreboard players set #dependency_error {namespace}.data 1"
+		encoder_command: str = f"scoreboard players set #dependency_error {ns}.data 1"
 		encoder_checks += check_version(config, lib_ns, value, encoder_command)
 
 		# Decoder check
@@ -151,7 +152,7 @@ execute if score #{namespace}.major load.status matches {major} if score #{names
 	if dependencies:
 		write_versioned_function(config, "load/check_dependencies", f"""
 ## Check if {config['project_name']} is loadable (dependencies)
-scoreboard players set #dependency_error {namespace}.data 0
+scoreboard players set #dependency_error {ns}.data 0
 {encoder_checks}
 """)
 
@@ -159,20 +160,20 @@ scoreboard players set #dependency_error {namespace}.data 0
 	if dependencies:
 		write_versioned_function(config, "load/valid_dependencies", f"""
 # Waiting for a player to get the game version, but stop function if no player found
-execute unless entity @p run schedule function {namespace}:v{version}/load/valid_dependencies 1t replace
+execute unless entity @p run schedule function {ns}:v{version}/load/valid_dependencies 1t replace
 execute unless entity @p run return 0
-execute store result score #game_version {namespace}.data run data get entity @p DataVersion
+execute store result score #game_version {ns}.data run data get entity @p DataVersion
 
 # Check if the game version is supported
-scoreboard players set #mcload_error {namespace}.data 0
-execute unless score #game_version {namespace}.data matches {DATA_VERSION}.. run scoreboard players set #mcload_error {namespace}.data 1
+scoreboard players set #mcload_error {ns}.data 0
+execute unless score #game_version {ns}.data matches {DATA_VERSION}.. run scoreboard players set #mcload_error {ns}.data 1
 
 # Decode errors
-execute if score #mcload_error {namespace}.data matches 1 run tellraw @a {{"text":"{config['project_name']} Error: This version is made for Minecraft {MINECRAFT_VERSION}+.","color":"red"}}
-execute if score #dependency_error {namespace}.data matches 1 run tellraw @a {{"text":"{config['project_name']} Error: Libraries are missing\\nplease download the right {config['project_name']} datapack\\nor download each of these libraries one by one:","color":"red"}}
+execute if score #mcload_error {ns}.data matches 1 run tellraw @a {{"text":"{config['project_name']} Error: This version is made for Minecraft {MINECRAFT_VERSION}+.","color":"red"}}
+execute if score #dependency_error {ns}.data matches 1 run tellraw @a {{"text":"{config['project_name']} Error: Libraries are missing\\nplease download the right {config['project_name']} datapack\\nor download each of these libraries one by one:","color":"red"}}
 {decoder_checks}
 # Load {config['project_name']}
-execute if score #game_version {namespace}.data matches 1.. if score #mcload_error {namespace}.data matches 0 if score #dependency_error {namespace}.data matches 0 run function {namespace}:v{version}/load/confirm_load
+execute if score #game_version {ns}.data matches 1.. if score #mcload_error {ns}.data matches 0 if score #dependency_error {ns}.data matches 0 run function {ns}:v{version}/load/confirm_load
 
 """)
 
