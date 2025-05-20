197cedd7f13c9865a1bf331aa3180d1584b9dca4
Stoupy51 <stoupy51@gmail.com>
2025-05-20 19:32:58 +0200

    build: 🚀 Updated to latest snapshot 1.21.6 (25w21a)
    - Bumped `DATAPACK_FORMAT` to 78 and `RESOURCE_PACK_FORMAT` to 62 to align with the latest Minecraft standards.
    - Updated `MINECRAFT_VERSION` to "25w21a" and `DATA_VERSION` to 4429 for compatibility with the current game version.

--- src/python_datapack/constants.py
+++ src/python_datapack/constants.py
@@ -3,10 +3,10 @@
 from .dependencies.bookshelf import BOOKSHELF_MODULES
 
 # Minecraft version constants
-DATAPACK_FORMAT: int = 71			# Pack format version, see https://minecraft.wiki/w/Pack_format#List_of_data_pack_formats
-RESOURCE_PACK_FORMAT: int = 55		# Resource pack format version, see https://minecraft.wiki/w/Pack_format#List_of_resource_pack_formats
-MINECRAFT_VERSION: str = "1.21.5"	# Text used when loading the datapack to warn the user when the data version is not right
-DATA_VERSION: int = 4321			# Depending on MC version, given by /data get entity @p DataVersion to check if the datapack is not running in an older version of MC
+DATAPACK_FORMAT: int = 78			# Pack format version, see https://minecraft.wiki/w/Pack_format#List_of_data_pack_formats
+RESOURCE_PACK_FORMAT: int = 62		# Resource pack format version, see https://minecraft.wiki/w/Pack_format#List_of_resource_pack_formats
+MINECRAFT_VERSION: str = "25w21a"	# Text used when loading the datapack to warn the user when the data version is not right
+DATA_VERSION: int = 4429			# Depending on MC version, given by /data get entity @p DataVersion to check if the datapack is not running in an older version of MC
 
 
 # Databases
